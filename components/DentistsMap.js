d696435eba0402dae8be44444c7598cf0787cae6
Rob Jackson <rob@rjackson.me>
2022-05-11 14:58:12 +0100

    zoom to search area

--- components/DentistsMap.js
+++ components/DentistsMap.js
@@ -1,10 +1,49 @@
-import { Circle, CircleMarker, MapContainer, Polygon, Popup, TileLayer } from "react-leaflet";
+import { Circle, CircleMarker, FeatureGroup, MapContainer, Polygon, Popup, TileLayer, useMap } from "react-leaflet";
 
 import "leaflet/dist/leaflet.css";
 import DentistInfo from "./DentistInfo";
 import { useDentistsState } from "contexts/Dentists";
 import { getCellsWithinGeoRadius } from "lib/dentists/core";
 import { h3ToGeoBoundary } from "h3-js";
+import { useEffect, useRef } from "react";
+
+const MapZoomer = ({ searchLocation, searchRadius }) => {
+  const map = useMap();
+  const searchArea = useRef();
+
+  // Zoom the map on search location / radius
+  useEffect(() => {
+    const bounds = searchArea.current.getBounds();
+    if (bounds.isValid()) {
+      map.fitBounds(bounds);
+    }
+  }, [map, searchLocation, searchRadius]);
+
+  return (
+    <FeatureGroup>
+      <Circle
+        ref={searchArea}
+        center={[searchLocation.lat, searchLocation.lng]}
+        radius={searchRadius * 1000}
+        pathOptions={{ color: "blue", fillOpacity: 0, dashArray: [10, 20] }}
+      />
+    </FeatureGroup>
+  );
+};
+
+const DentistCircle = ({ dentist }) => {
+  const acceptingAnyPatients = Object.values(dentist.AcceptingPatients).some((v) => v);
+  return (
+    <CircleMarker
+      center={[dentist.Latitude, dentist.Longitude]}
+      pathOptions={{ color: acceptingAnyPatients ? "blue" : "gray" }}
+    >
+      <Popup>
+        <DentistInfo dentist={dentist} />
+      </Popup>
+    </CircleMarker>
+  );
+};
 
 const Map = ({ showCells = false, resolution }) => {
   const { dentists, searchLocation, searchRadius } = useDentistsState();
@@ -14,7 +53,6 @@ const Map = ({ showCells = false, resolution }) => {
   // Geographic centre of GB - https://en.wikipedia.org/wiki/Centre_points_of_the_United_Kingdom#Great_Britain
   const center = [54.01, -2.33];
   const zoom = 7;
-
   return (
     <>
       <MapContainer center={center} zoom={zoom} scrollWheelZoom={true} preferCanvas={true}>
@@ -22,28 +60,14 @@ const Map = ({ showCells = false, resolution }) => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {showCells && (
-          <>
-            <Circle
-              center={[searchLocation.lat, searchLocation.lng]}
-              radius={searchRadius * 1000}
-              pathOptions={{ color: "yellow", fillOpacity: 0 }}
-            />
-            {cellPolygons.map((poly, i) => (
-              <Polygon key={i} positions={poly} pathOptions={{ color: "red", fillOpacity: 0.25, opacity: 0.25 }} />
-            ))}
-          </>
-        )}
+        <MapZoomer searchLocation={searchLocation} searchRadius={searchRadius} />
+        {showCells &&
+          cellPolygons.map((poly, i) => (
+            <Polygon key={i} positions={poly} pathOptions={{ color: "red", fillOpacity: 0.25, opacity: 0.25 }} />
+          ))}
         {dentists.map(
           (dentist) =>
-            dentist.Latitude &&
-            dentist.Longitude && (
-              <CircleMarker key={dentist.ODSCode} center={[dentist.Latitude, dentist.Longitude]}>
-                <Popup>
-                  <DentistInfo dentist={dentist} />
-                </Popup>
-              </CircleMarker>
-            )
+            dentist.Latitude && dentist.Longitude && <DentistCircle key={dentist.ODSCode} dentist={dentist} />
         )}
       </MapContainer>
       <style>
