9a611b49a0b17ee9bebde1e98fa704dd671e5bd8
Rob Jackson <rob@rjackson.me>
2022-05-11 19:16:39 +0100

    polish button

--- components/Button.js
+++ components/Button.js
@@ -6,20 +6,27 @@ py-1
 rounded
 
 ring-2
-ring-indigo-500
+ring-gray-400
+border-2
+border-transparent
 
+hover:ring-indigo-500
+hover:border-white
 hover:bg-indigo-500
 hover:text-white
 
 focus:outline-none
-focus:ring
+focus:ring-indigo-500
+focus:border-white
 focus:bg-indigo-500
 focus:text-white
 
 dark:hover:bg-fuchsia-400
+dark:hover:border-gray-900
+dark:hover:ring-fuchsia-400
 dark:focus:bg-fuchsia-400
+dark:focus:border-gray-900
 dark:focus:ring-fuchsia-400
-dark:ring-fuchsia-600
 dark:bg-gray-900
 `;
 
